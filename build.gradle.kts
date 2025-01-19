2bc3e53198337878f3ac8569b13b0cf4b38ea045
Sandip Chitale <sandipchitale@gmail.com>
2025-01-19 18:51:13 -0500

    Update for 2025.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.sandipchitale"
-version = "1.0.34"
+version = "1.0.35"
 
 repositories {
     mavenCentral()
@@ -38,7 +38,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("241")
-        untilBuild.set("243.*")
+        untilBuild.set("251.*")
     }
 
     signPlugin {
